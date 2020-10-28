61fe0b7d97c3532a52354e68f8934a3a9051407a
Favre Valentin <61196897+Valentinfavre@users.noreply.github.com>
2020-10-28 16:06:15 +0100

    Initial commit

---
+++ src/users-data.js
@@ -0,0 +1,39 @@
+// Data export
+export default [
+  {
+    name: "😃 William",
+    location: "🏘️ Lagos",
+    car: "🚘 Honda",
+    passion: "Bicycle"
+  },
+  {
+    name: "😃 Chris",
+    location: "🏘️ Moon",
+    car: "🚘 Tesla",
+    passion: "Eat some Pasta"
+  },
+  {
+    name: " 😃 Rose",
+    location: "🏘️ Venice",
+    car: "🚘 Pagani",
+    passion: "Football show"
+  },
+  {
+    name: "😃 Mike",
+    location: "🏘️ Milan",
+    car: "🚘 Rolls Royce",
+    passion: "run boy run"
+  },
+  {
+    name: "😃 Liz",
+    location: "🏘️ Beirut",
+    car: "🚘 Mercedes",
+    passion: "go to Space"
+  },
+  {
+    name: "Valentin",
+    location: "Pralognan La vanoise",
+    car: "Marrusia B2",
+    passion: "Eat / Program / Repeat"
+  }
+];
